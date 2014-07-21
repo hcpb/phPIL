34ac05c269d75d5d041850991bcf59ecab9131b8
r14793 <r14793@r14793ubuntu.(none)>
2014-07-21 14:27:07 -0600

    got folder watch working, can generate print image and place in print folder...

--- pbprint.py
+++ pbprint.py
@@ -1,28 +1,57 @@
 #!/usr/bin/python
 # many dependencies are all brought in with this import...
 from photoboothlib import *
-
+from string import split
 
 #=============================================================================
 # ==================================  MAIN  ==================================
 #=============================================================================
 
-# list of already printed files...
+# lists used below...
 printed = []
+existing = []
 
 # change directory to shared directory root of photo booth files...
+#os.chdir('/volumes/PHOTOBOOTH')
+os.chdir('/home/r14793/photobooth/')
+# add filenames to existing list and sort...
+#for i in os.listdir('for-display'):
+#	print i,
+#	if os.path.isfile('for-display/'+i): existing.append(i)
+existing = os.listdir('for-display')
+existing.sort()
+print repr(existing)
+print
+filename = split(existing[-1], '_')[0]
+print 'filename:', filename
 
 while (1):
 
 	# watch for new file in for-display
-	#   something here...
-	# then grab same numbered file in for-phone 
-	#   something more here... 
-	#   strip all but "filename" of newest session...
+	new = False
+	while not(new):
+		tmp = os.listdir('for-display')
+		tmp.sort()
+		newest = tmp[-1]
+		if newest not in existing:
+			new = True # so we fall out of while loop...
+			existing.append(newest)
+		else:
+			time.sleep(2)
 
 	start = time.time()
+
+	# grab the actual single strip filename, which may have a color suffix...
+	tmp = os.listdir('for-phone')
+	tmp.sort()
+	filename = tmp[-1]
+	outputfilename = split(filename, '_')[0]+'_print'
+	if '-' not in filename: outputfilename = outputfilename + '.jpg'
+	else: outputfilename = outputfilename + '-' + split(filename, '-')[1]
+	print newest, filename, outputfilename
 	
-	imPrint = Image.open('for-phone/'+filename+'_phone.jpg')
+	print '\r\nCreating double print strip...'
+	imPrint = Image.open('for-phone/'+filename)
 	imDouble = Image.new('RGB', (4000, 6000), 'white')
 	# generate double strip for printing...
 	imDouble.paste( imPrint, (0, 0, 2000, 6000) )
@@ -30,7 +59,7 @@ while (1):
 	draw = ImageDraw.Draw(imDouble)
 	draw.line( (2000, 0, 2000, 6000), fill='rgb(0,0,0)', width=2)
 	del draw
-	imDouble.save('for-print/'+filename+'_print.jpg', 'JPEG', quality=90)
+	imDouble.save('for-print/'+outputfilename, 'JPEG', quality=90)
 
 	print '\r\nProcessing done: ', time.time()-start
 
